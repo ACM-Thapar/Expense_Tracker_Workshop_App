aed0c89d9cd934fdb2faac9f7562b35c9907e3ae
AYUSH KUMAR SINGH <ayushkumarsingh0708@gmail.com>
2022-10-02 22:46:22 +0530

    google auth added with firebase

--- lib/transactions.dart
+++ lib/transactions.dart
@@ -1,19 +1,15 @@
 class Transaction {
-
   String desc;
 
   int amount;
 
   Transaction({required this.amount, required this.desc});
 
-  Transaction.fromMap(Map map):
-      this.desc = map['desc'],
-      this.amount = map['amount'];
+  Transaction.fromMap(Map map)
+      : desc = map['desc'],
+        amount = map['amount'];
 
-  Map toMap(){
-    return{
-      'desc':this.desc,
-      'amount':this.amount
-    };
+  Map toMap() {
+    return {'desc': desc, 'amount': amount};
   }
 }
